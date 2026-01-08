cc6969e96fe9f43fa8fe952a50bbce908a2241d2
itokagimaru <cryptomeria.rookie@gmail.com>
2026-01-08 11:11:52 +0900

    Add elemental damage reduction to player and entity
    Introduced FIRE_DMG_REDUCE, WATER_DMG_REDUCE, and NATURE_DMG_REDUCE to PlayerStatus and EntityData for tracking elemental damage reduction rates. Updated base status initialization and entity keys to support these new attributes, improving extensibility for elemental effects.

--- src/main/java/io/github/itokagimaru/artifact/data/EntityData.java
+++ src/main/java/io/github/itokagimaru/artifact/data/EntityData.java
@@ -1,5 +1,6 @@
 package io.github.itokagimaru.artifact.data;
 
+import io.github.itokagimaru.artifact.utils.ByteArrayConverter;
 import org.bukkit.NamespacedKey;
 
 public class EntityData {
@@ -9,7 +10,10 @@ public class EntityData {
         return new NamespacedKey(NAMESPACE, key);
     }
     public static final IntKey PHASE = new IntKey(getKey("phase"), () -> 0);
-    public static final IntKey ATK = new IntKey(getKey("atk"), () -> 0);
-    public static final IntKey DEF = new IntKey(getKey("def"), () -> 0);
+    public static final IntKey ENEMY_ATK = new IntKey(getKey("atk"), () -> 0);
+    public static final IntKey ENEMY_DEF = new IntKey(getKey("def"), () -> 0);
+    public static final ByteArrayKey ENEMY_FIRE_DMG_REDUCE = new ByteArrayKey(getKey("f_reduce"), () -> ByteArrayConverter.toByte(0.0));
+    public static final ByteArrayKey ENEMY_WATER_DMG_REDUCE = new ByteArrayKey(getKey("w_reduce"), () -> ByteArrayConverter.toByte(0.0));
+    public static final ByteArrayKey ENEMY_NATURE_DMG_REDUCE = new ByteArrayKey(getKey("n_reduce"), () -> ByteArrayConverter.toByte(0.0));
 
 }
