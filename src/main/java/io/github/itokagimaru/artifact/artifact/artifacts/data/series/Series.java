ca0b102ad898a4d52d1c7dcac75a878acb8700cc
Kuma_junk <kumajunk.mc@gmail.com>
2026-01-09 14:10:02 +0900

    feat: テスト用のArtifactSeriesを追加

--- src/main/java/io/github/itokagimaru/artifact/artifact/artifacts/data/series/Series.java
+++ src/main/java/io/github/itokagimaru/artifact/artifact/artifacts/data/series/Series.java
@@ -2,6 +2,7 @@ package io.github.itokagimaru.artifact.artifact.artifacts.data.series;
 
 import io.github.itokagimaru.artifact.artifact.artifacts.series.Base.BaseArtifact;
 import io.github.itokagimaru.artifact.artifact.artifacts.series.ExRookie.ExRookie;
+import io.github.itokagimaru.artifact.artifact.artifacts.series.King.King;
 import io.github.itokagimaru.artifact.artifact.artifacts.series.Rookie.Rookie;
 
 import java.util.HashMap;
@@ -9,8 +10,9 @@ import java.util.function.Supplier;
 
 public class Series {
     public static enum artifactSeres{
-        ROOKIE(0, () -> new Rookie()),
-        ROOKIE_EX(1,() -> new ExRookie());
+        ROOKIE(0, Rookie::new),
+        ROOKIE_EX(1, ExRookie::new),
+        KING(2, King::new);
         public final int getId;
         private final Supplier<BaseArtifact> supplier;
         artifactSeres(int id, Supplier<BaseArtifact> supplier){
