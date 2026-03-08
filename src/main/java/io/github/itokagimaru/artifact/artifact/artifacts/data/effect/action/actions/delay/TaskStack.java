5566c55e18fe70bbbacf2c6b11af15da0209036d
itokagimaru <cryptomeria.rookie@gmail.com>
2026-03-08 21:53:53 +0900

    delay回りの改修
    delayに識別子をつけれるように
    cancel-delayで指定した識別子を持つdelayをキャンセルできるように

--- src/main/java/io/github/itokagimaru/artifact/artifact/artifacts/data/effect/action/actions/delay/TaskStack.java
+++ src/main/java/io/github/itokagimaru/artifact/artifact/artifacts/data/effect/action/actions/delay/TaskStack.java
@@ -2,34 +2,51 @@ package io.github.itokagimaru.artifact.artifact.artifacts.data.effect.action.act
 
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TaskStack {
-    private static HashMap<UUID, List<BukkitTask>> tasksMap = new HashMap<UUID, List<BukkitTask>>();
+    private static final HashMap<UUID, List<DelayTask>> tasksMap = new HashMap<UUID, List<DelayTask>>();
 
-    public static List<BukkitTask> getTasks(UUID playerUUID) {
+    public static List<DelayTask> getTasks(UUID playerUUID) {
         return tasksMap.get(playerUUID);
     }
 
-    public static void setTasks(UUID playerUUID, List<BukkitTask> tasks) {
+    public static void setTasks(UUID playerUUID, List<DelayTask> tasks) {
         tasksMap.put(playerUUID, tasks);
     }
 
-    public static void setTasks(UUID playerUUID, BukkitTask task) {
-        List<BukkitTask> tasks = tasksMap.get(playerUUID);
-        if (tasks == null) tasks = new ArrayList<BukkitTask>();
+    public static void setTasks(UUID playerUUID, DelayTask task) {
+        List<DelayTask> tasks = tasksMap.get(playerUUID);
+        if (tasks == null) tasks = new ArrayList<DelayTask>();
         tasks.add(task);
         setTasks(playerUUID, tasks);
     }
 
-    public static void cancelTasks(UUID playerUUID){
-        List<BukkitTask> tasks = tasksMap.get(playerUUID);
+    public static void cancelAllTasks(UUID playerUUID){
+        List<DelayTask> tasks = tasksMap.get(playerUUID);
         if(tasks==null || tasks.isEmpty()) return;
-        for(BukkitTask task : tasks){
+        for(DelayTask task : tasks){
             task.cancel();
+        }
+        tasksMap.remove(playerUUID);
+    }
+
+    public static void cancelTasks(UUID playerUUID, String key){
+        List<DelayTask> tasks = tasksMap.get(playerUUID);
+        if(tasks == null || tasks.isEmpty()) return;
+
+        Iterator<DelayTask> it = tasks.iterator();
+
+        while(it.hasNext()){
+            DelayTask task = it.next();
+            if(key.equals(task.getKey())){
+                task.cancel();
+                it.remove();
+            }
+        }
+
+        if(tasks.isEmpty()){
+            tasksMap.remove(playerUUID);
         }
     }
 }
